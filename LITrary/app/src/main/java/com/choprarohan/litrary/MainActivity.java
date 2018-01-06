98246948da711436a39e4b051ad1ef7e15e5c6d5
Rohan Chopra <09rohanchopra@gmail.com>
2018-01-06 17:27:13 +0530

    Start with android app

---
+++ LITrary/app/src/main/java/com/choprarohan/litrary/MainActivity.java
@@ -0,0 +1,21 @@
+package com.choprarohan.litrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
